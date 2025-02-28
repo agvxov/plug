13a6628828f0d64797ab338921baa0066a40a153
anon <anon@anon.anon>
2025-02-28 23:21:13 +0100

    simplified tests

--- test/CMDTEST_plug.rb
+++ test/CMDTEST_plug.rb
@@ -14,69 +14,45 @@ class CMDTEST_plug < Cmdtest::Testcase
   end
 
   def test_simple_unplug
+    import_file "test/test_collapsed.txt", "./"
     import_file "test/test_expended.txt", "./"
-    expected = [
-      "original text",
-      "#placeholder<hw> COLLAPSED",
-      "some more original text",
-    ]
 
     cmd "plug test_expended.txt -a -u" do
       changed_files ["test_expended.txt"]
-      file_equal "test_expended.txt", expected
+      file_equal "test_expended.txt", File.read("test_collapsed.txt")
       exit_zero
     end
   end
 
   def test_simple_plug
     import_file "test/test_collapsed.txt", "./"
-    expected = [
-      "original text",
-      "#placeholder<hw> BEGIN",
-      "hello world",
-      "#placeholder<hw> END",
-      "some more original text",
-    ]
+    import_file "test/test_expended.txt", "./"
 
     cmd "plug test_collapsed.txt -d hw 'hello world' -g" do
       changed_files ["test_collapsed.txt"]
-      file_equal "test_collapsed.txt", expected
+      file_equal "test_collapsed.txt", File.read("test_expended.txt")
       exit_zero
     end
   end
 
   def test_simple_unplug_2
+    import_file "test/test2_collapsed.cpp", "./"
     import_file "test/test2_expended.cpp", "./"
-    expected = [
-      "#include <stdio.h>",
-      "signed main(void){",
-      "\t// #placeholder<code> COLLAPSED",
-      "\treturn 0;",
-      "}",
-    ]
 
     cmd "plug test2_expended.cpp -a -u" do
       changed_files ["test2_expended.cpp"]
-      file_equal "test2_expended.cpp", expected
+      file_equal "test2_expended.cpp", File.read("test2_collapsed.cpp")
       exit_zero
     end
   end
 
   def test_simple_plug_2
     import_file "test/test2_collapsed.cpp", "./"
-    expected = [
-      "#include <stdio.h>",
-      "signed main(void){",
-      "\t// #placeholder<code> BEGIN",
-      "\tputs(\"hello world\");",
-      "\t// #placeholder<code> END",
-      "\treturn 0;",
-      "}",
-    ]
+    import_file "test/test2_expended.cpp", "./"
 
     cmd "plug test2_collapsed.cpp -d code '\tputs(\"hello world\");' -g" do
       changed_files ["test2_collapsed.cpp"]
-      file_equal "test2_collapsed.cpp", expected
+      file_equal "test2_collapsed.cpp", File.read("test2_expended.cpp")
       exit_zero
     end
   end
--- test/test2_expended.cpp
+++ test/test2_expended.cpp
@@ -1,7 +1,7 @@
 #include <stdio.h>
 signed main(void){
 	// #placeholder<code> BEGIN
-puts("hello world")
+	puts("hello world");
 	// #placeholder<code> END
 	return 0;
 }
